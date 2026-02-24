e5c0ea02a06d4ec679223080107d139454da86a8
NOEMI GP <noemigp0@gmail.com>
2026-02-24 13:24:53 -0600

    Modifying JSON response at login

--- src/main/java/com/loyalt/loyalt/dto/AuthResponse.java
+++ src/main/java/com/loyalt/loyalt/dto/AuthResponse.java
@@ -1,12 +1,24 @@
 package com.loyalt.loyalt.dto;
 
+import com.loyalt.loyalt.model.entity.AppUser;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.Date;
+
 public class AuthResponse {
     private String token;
     private String role;
+    private String type;
+    private Long expiresIn;
 
-    public AuthResponse(String token, String role){
+    private UserInfoResponse userInfoResponse;
+
+    public AuthResponse(String token, String role, String type, Long expiresIn, UserInfoResponse userInfoResponse){
         this.token = token;
         this.role = role;
+        this.expiresIn = expiresIn;
+        this.userInfoResponse = userInfoResponse;
+        this.type = type;
     }
 
     public String getToken() {
@@ -15,5 +27,17 @@ public class AuthResponse {
 
     public String getRole() {
         return role;
+    }
+
+    public Long getExpiresIn() {
+        return expiresIn;
+    }
+
+    public UserInfoResponse getUserInfoResponse() {
+        return userInfoResponse;
+    }
+
+    public String getType() {
+        return type;
     }
 }
--- src/main/java/com/loyalt/loyalt/service/AuthService.java
+++ src/main/java/com/loyalt/loyalt/service/AuthService.java
@@ -3,6 +3,7 @@ package com.loyalt.loyalt.service;
 import com.loyalt.loyalt.dto.AuthRequest;
 import com.loyalt.loyalt.dto.AuthResponse;
 import com.loyalt.loyalt.dto.RegisterRequest;
+import com.loyalt.loyalt.dto.UserInfoResponse;
 import com.loyalt.loyalt.exception.InvalidEmailOrPasswordException;
 import com.loyalt.loyalt.exception.UserAlreadyExistsException;
 import com.loyalt.loyalt.model.entity.AppUser;
@@ -15,9 +16,12 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Date;
 
 @Service
 public class AuthService {
@@ -47,8 +51,13 @@ public class AuthService {
 
         AppUser user = appUserRepository.findByEmail(request.getEmail())
                 .orElseThrow();
+
         String token = jwtService.generateToken(user);
-        return new AuthResponse(token, user.getRole().name());
+
+        long expiration = 1000 * 60 * 60;
+        UserInfoResponse userInfo = new UserInfoResponse(user.getId(), user.getEmail(), user.getRole());
+
+        return new AuthResponse(token, user.getRole().name(), "Bearer", expiration , userInfo);
     }
 
     public void register(RegisterRequest registerRequest) {
