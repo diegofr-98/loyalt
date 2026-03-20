1b890052f004c81afbabd60a9e347c9593619055
NOEMI GP <noemigp0@gmail.com>
2026-03-20 10:56:59 -0600

    Adding annotation to solve annotation warning

--- src/main/java/com/loyalt/loyalt/LoyaltApplication.java
+++ src/main/java/com/loyalt/loyalt/LoyaltApplication.java
@@ -2,10 +2,12 @@ package com.loyalt.loyalt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration.class
 })
+@EnableSpringDataWebSupport(pageSerializationMode = EnableSpringDataWebSupport.PageSerializationMode.VIA_DTO)
 public class LoyaltApplication {
 
 
